3ed96b57e9967219c9182fe0e2107584d6e679cc
Ana Rapp <zmraxton@gmail.com>
2022-03-08 18:32:26 -0700

    water elemental can spawn

---
+++ NPCs/WaterElemental.cs
@@ -0,0 +1,28 @@
+﻿using ClickerClass.Items;
+using ClickerClass.Items.Accessories;
+using ClickerClass.Items.Weapons.Clickers;
+using ClickerClass.Utilities;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ClickerClass.NPCs
+{
+	public class WaterElemental : ModNPC
+	{
+		public WaterElemental()
+		{
+		}
+
+		public static bool SpawnNPC(int x, int y, int playerID)
+		{
+			if(Main.player[playerID].ZoneBeach && Main.rand.Next(16) == 1)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
